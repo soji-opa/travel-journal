66ca73343c0b20e118ef589694c3841634aa731c
Azeez Olasoji <azeezolasoji@gmail.com>
2022-11-30 15:22:02 +0100

    Finished styling the mobile & destop view, left with the non-responsive image calls

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import Card from './components/Card';
 import Header from './components/Header';
+import data from '../src/data';
 
 function App() {
+	const card = data.map((item) => (
+		<Card
+			key={item.id}
+			title={item.title}
+			location={item.location}
+			link={item.googleMapsUrl}
+			description={item.description}
+			image={item.imageUrl}
+			startdate={item.startDate}
+			enddate={item.endDate}
+		/>
+	));
 	return (
 		<div className='App'>
 			<Header />
-			<Card />
+			<div className='card-list'>{card}</div>
 		</div>
 	);
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,27 @@
 import japanimage from '../images/japanimage.png';
 import geolocation from '../images/geolocation-icon.png';
 
-export default function Card() {
+export default function Card(props) {
 	return (
 		<div className='card'>
-			<div>
-				<img src={japanimage} className='card-img' />
+			<div className='image--div'>
+				<img src={props.image} className='card-img' />
 			</div>
 			<div className='card--description'>
 				<div className='card-location'>
 					<img src={geolocation} />
-					<h2 className='description-title'>JAPAN</h2>
-					<a href='#'> View on Google maps</a>
+					<h2 className='description-title'>{props.location}</h2>
+					<a href={props.link} target='_blank'>
+						View on Google maps
+					</a>
 				</div>
-				<h3 className='location-title'>Mount Fuji</h3>
+				<h3 className='location-title'>{props.title}</h3>
 
-				<p className='card-date'>12 Jan, 2021 - 24 Jan, 2021</p>
-
-				<p className='card-description'>
-					Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-					(12,380 feet). Mount Fuji is the single most popular tourist site in
-					Japan, for both Japanese and foreign tourists.'
+				<p className='card-date'>
+					{props.startdate} - {props.enddate}
 				</p>
+
+				<p className='card-text'>{props.description}</p>
 			</div>
 		</div>
 	);
